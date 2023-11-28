689af815a56921621b5ad20903f274c05ef864ed
Brandon <fan.brandon@gmail.com>
2023-11-27 21:04:17 -0800

    pushing the index.php - BF

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="style.css"/>
+    <title>Hornet Management System</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="logo-container">
+        <img src="images/Primary_Vertical_1_Color_Green.png" frameborder="0" />
+      </div>
+      <div class="waviy">
+        <!-- Use the same class for all text elements, and set a different custom attribute for animation delay -->
+        <div>
+          <span
+            class="text"
+            data-animation-delay="0"
+            style="font-family: sans-serif"
+            >Hornet</span
+          >
+        </div>
+        <div>
+          <span
+            class="text"
+            data-animation-delay="0"
+            style="font-family: sans-serif"
+            >Management</span
+          >
+        </div>
+        <div>
+          <span
+            class="text"
+            data-animation-delay="0"
+            style="font-family: sans-serif"
+            >System</span
+          >
+        </div>
+        <div class="center">
+          <a href="StudentLogin.php" class="button">Student Login</a>
+          <a href="adminLogin.php" class="button">Admin Login</a>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
