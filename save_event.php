e1089a3a959c7d7ae403c5e3db84c41e18a4f7a7
Brandon <fan.brandon@gmail.com>
2023-11-29 21:00:46 -0800

    add save event file -BF

---
+++ save_event.php
@@ -0,0 +1,24 @@
+<?php
+include("database.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    $event_name = mysqli_real_escape_string($con, $data['event_name']);
+    $event_date = mysqli_real_escape_string($con, $data['event_date']);
+    $event_location = mysqli_real_escape_string($con, $data['event_location']);
+    $event_description = mysqli_real_escape_string($con, $data['event_description']);
+
+    // Insert into the 'events' table
+    $insert_query = "INSERT INTO events (event_name, event_date, event_location, event_description) 
+                     VALUES ('$event_name', '$event_date', '$event_location', '$event_description')";
+
+    if (mysqli_query($con, $insert_query)) {
+        echo json_encode(array('success' => true, 'message' => 'Event added successfully!'));
+    } else {
+        echo json_encode(array('success' => false, 'message' => 'Error: ' . mysqli_error($con)));
+    }
+} else {
+    echo json_encode(array('success' => false, 'message' => 'Invalid request'));
+}
+?>
